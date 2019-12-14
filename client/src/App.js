64493912ebdc00e3a705b272383142c6e51bff6e
hzoou <woojooo52@naver.com>
2019-12-14 19:40:06 +0900

    chore: 주석 추가
    바질 토스트님의 친절한 주석을 베이스로 약간의 주석을 더했습니다.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { ControlledEditor } from '@monaco-editor/react';
 import io from 'socket.io-client';
 import { Operation, merge } from './ot';
 
+// 임시 주소
 const target = `http://127.0.0.1:3030`;
 const live = {};
 const url = 'basiltoast';
@@ -10,27 +11,39 @@ const url = 'basiltoast';
 function App() {
     const [code, setCode] = useState('');
     const [value, setValue] = useState('');
+
+    // 자신의 요청이 되돌아오기 전까지 이벤트를 막는다.
     const [isPending, setIsPending] = useState(false);
+
+    // code변경 시 사용자의 키 입력이 아닌 다른경우 이벤트를 막기 위한 flag
     const [isBusy, setIsBusy] = useState(false);
+
     const monacoRef = useRef();
+
+    // 소켓 연결을 한 뒤, 이벤트를 정의할 때 현재 변경된 코드를 받아오기 위한 레퍼런스
     const codeRef = useRef('');
+
+    // 사용자의 커서를 이동시키기 위한 레퍼런스
     const cursorRef = useRef();
 
     const handleInit = useCallback(() => {
+        // 소켓 연결
         live.socket = io(target);
         const { socket } = live;
         socket.on('connection', id => {
             live.id = id;
             socket.emit('join', url, code);
         });
-        socket.on('initailize', initailData => {
+
+        // 연결 후 프로젝트 정보 초기화
+        socket.on('initialize', initialData => {
             setIsBusy(true);
-            setCode(initailData);
+            setCode(initialData);
         });
+
+        // 다른 클라이언트에서 에디터 내용을 변경
         socket.on('change', (id, op) => {
-            if (id === socket.id) {
-                return setTimeout(setIsPending(false), 0);
-            }
+            if (id === socket.id) return setTimeout(setIsPending(false), 0);
             setIsBusy(true);
             if (!monacoRef.current) return;
             cursorRef.current = monacoRef.current.getSelection();
@@ -47,15 +60,13 @@ function App() {
     });
 
     useEffect(() => {
-        if (code === value) return;
-        if (isPending || isBusy) return;
-        if (!live.socket) return;
+        if (code === value || isPending || isBusy || !live.socket) return;
         live.socket.emit('change', new Operation(code, value));
         setIsBusy(true);
+        setCode(value);
+
         if (!monacoRef.current) return;
         cursorRef.current = monacoRef.current.getSelection();
-        setCode(value);
-        setIsPending(true);
     }, [code, value, isPending, isBusy]);
 
     useEffect(() => {
@@ -64,24 +75,18 @@ function App() {
 
     useEffect(() => {
         if (!monacoRef.current) return;
-        const selection = monacoRef.current.getSelection();
-        console.log('before : ', cursorRef.current);
-        console.log('after  : ', selection);
-
-        // Object.keys(monaco)
-
-        // const newCursor = {
-        //     endColumn: 4,
-        //     endLineNumber: 1,
-        //     positionColumn: 4,
-        //     positionLineNumber: 1,
-        //     selectionStartColumn: 1,
-        //     selectionStartLineNumber: 1,
-        //     startColumn: 1,
-        //     startLineNumber: 1
-        // };
-
         codeRef.current = code;
+
+        // 코드 변경 이전의 커서 좌표
+        const before = cursorRef.current;
+        // 코드 변경 이후의 커서 좌표
+        const after = monacoRef.current.getSelection();
+
+        // 본인이 작성한 경우에는 커서의 위치가 자동으로 변경되기 때문에 커서를 이동할 필요가 없음
+        if (JSON.stringify(before) === JSON.stringify(after)) return;
+
+        // 커서 이동
+        // monacoRef.current.setSelection();
     }, [code]);
 
     useEffect(handleInit, []);
